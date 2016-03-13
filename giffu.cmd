4e72e4e928a223063c22a83f67ca306e748a615e
paperslove <toiletpaper9000@windowslive.com>
2016-03-13 07:04:57 +0200

    Update giffu.cmd
    updated to make it not retarded

--- giffu.cmd
+++ giffu.cmd
@@ -1,2 +1,5 @@
-ffmpeg -i input.gif -vf palettegen palette.png
-ffmpeg -i input.gif -i palette.png -lavfi "scale=300:-1,paletteuse" output.gif
+@echo off
+set /p our_input="Enter filename WITH file extension: "
+set /p our_output="Enter filename to be saved as (no extension): "
+ffmpeg -i "%our_input%" -vf palettegen palette.png
+ffmpeg -i "%our_input%" -i palette.png -lavfi "scale=-1:-1,paletteuse" "%our_output%.gif"
